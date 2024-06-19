c35e8be7af53ae6de7748cbb59551e96a0192785
askumardev <askumar.dev@outlook.com>
2024-06-19 17:13:06 +0530

    spec updates

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,14 +1,12 @@
 import { Controller } from "@hotwired/stimulus";
 // import * as L from "https://cdn.jsdelivr.net/npm/leaflet@1.8.0/dist/leaflet-src.esm.js";
 
-// Connects to data-controller="map"
+
 export default class extends Controller {
   static targets = ["map"]
 
   connect() {
-    // Initialize the map
     const map = L.map('map')
-    // Add OpenStreetMap tile layer
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
@@ -17,7 +15,6 @@ export default class extends Controller {
     map.setView([40.801, -73.95], 8);
     var mkr = L.marker([40.801, -73.95]);
     mkr.addTo(map);
-    // Parse WKT and add to the map
     const wellKnownText = this.mapTarget.dataset.wkt;
     console.log(wellKnownText);
     L.polygon( [[40.8100, -73.9580], [40.8007, -73.9298], [40.7644, -73.9498], [40.7644, -73.9580]]).addTo(map);
