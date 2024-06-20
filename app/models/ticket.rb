86ffca01b86e98b63ebc2389ef502616ba3f39db
askumardev <askumar.dev@outlook.com>
2024-06-20 11:58:11 +0530

    show page update

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -5,9 +5,6 @@ class Ticket < ApplicationRecord
   def self.convert_polygon(original_polygon)
     coordinates_str = original_polygon.match(/\(\((.*)\)\)/)[1]
     coordinates = coordinates_str.split(", ").map { |coord| coord.split(" ").map(&:to_f) }
-    # transformers = coordinates.map { |lon, lat| [ lon.round(3), lat.round(3) ] }
     coordinates.map { |lon, lat| [ lon.round(3), lat.round(3) ] }
-    # transformers.map { |lon, lat| "#{lon} #{lat}" }.join(", ")
-    # "POLYGON((" + transformers.map { |lon, lat| "#{lon} #{lat}" }.join(", ") + "))"
   end
 end
